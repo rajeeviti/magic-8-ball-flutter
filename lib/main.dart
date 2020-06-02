76e97e8cb93a37569688f8991729b1740d65264a
rajeeviti <rajeev@oneseeker.com>
2020-06-02 21:48:56 +0530

    magic-ball-app
    Ask any question and it shall be answered for you. May the Force be with You.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main(){
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: Center(child: Text('Ask Me Anything')),
+          backgroundColor: Colors.indigo,
+        ),
+        body: Ball(),
+      ),
+    ),
+  );
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+
+  int ballNumber = 1;
+
+  void getBallNumber(){
+    setState(() {
+      ballNumber = Random().nextInt(5)+1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: FlatButton(
+          child: Image.asset('images/ball$ballNumber.png'),
+          onPressed: () {
+            getBallNumber();
+          },
+        ),
       ),
     );
+  }
+}
